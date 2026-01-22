b1db379c7b388a00ed37629177e54a37ddc7004f
csnewcs <cs.newcs0@gmail.com>
2026-01-22 11:14:18 +0900

    init

---
+++ Config.go
@@ -0,0 +1,6 @@
+package main
+
+type Config struct {
+	Token   string `json:"token"`
+	AdminId string `json:"adminId"`
+}
---
+++ Main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+
+}
